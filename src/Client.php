5b9d71a0ece7518d2babf79bf3ffbf9c1bc681f8
odin <odinuv@gmail.com>
2019-05-07 13:57:31 +0200

    initial import

---
+++ src/Client.php
@@ -0,0 +1,140 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\JobQueueInternalClient;
+
+use Closure;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Request;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\Constraints\Url;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\Validation;
+
+class Client
+{
+
+    /**
+     * @var GuzzleClient
+     */
+    protected $guzzle;
+
+    private const DEFAULT_USER_AGENT = 'Internal PHP Client';
+    private const DEFAULT_BACKOFF_RETRIES = 10;
+
+    public function __construct(string $url, string $token, array $options = [])
+    {
+        $validator = Validation::createValidator();
+        $errors = $validator->validate($url, [new Url([])]);
+        $errors->addAll($validator->validate($token, [new NotBlank([])]));
+        if (!empty($options['backoffMaxTries'])) {
+            $errors->addAll($validator->validate($token, [new Range(['min' => 0, 'max' => 100])]));
+            $options['backoffMaxTries'] = intval($options['backoffMaxTries']);
+        } else {
+            $options['backoffMaxTries'] = self::DEFAULT_BACKOFF_RETRIES;
+        }
+        if (!empty($options['userAgent'])) {
+            $options['userAgent'] = (string) $options['userAgent'];
+        } else {
+            $options['userAgent'] = self::DEFAULT_USER_AGENT;
+        }
+        if ($errors->count() !== 0) {
+            $messages = '';
+            /** @var ConstraintViolationInterface $error */
+            foreach ($errors as $error) {
+                $messages .= $error->getMessage() . "\n";
+            }
+            throw new ClientException('Invalid parameters when creating client: ' . $messages);
+        }
+        $this->guzzle = $this->initClient($url, $token, $options);
+    }
+
+    private function initClient(string $url, string $token, array $options = []): GuzzleClient
+    {
+        // Initialize handlers (start with those supplied in constructor)
+        if (isset($options['handler']) && $options['handler'] instanceof HandlerStack) {
+            $handlerStack = HandlerStack::create($options['handler']);
+        } else {
+            $handlerStack = HandlerStack::create();
+        }
+        // Set exponential backoff
+        $handlerStack->push(Middleware::retry(self::createDefaultDecider($options['backoffMaxTries'])));
+        // Set handler to set default headers
+        $handlerStack->push(Middleware::mapRequest(
+            function (RequestInterface $request) use ($token, $options) {
+                return $request
+                    ->withHeader('User-Agent', $options['userAgent'])
+                    ->withHeader('X-InternalApi-Token', $token)
+                    ->withHeader('Content-type', 'application/json');
+            }
+        ));
+        // Set client logger
+        if (isset($options['logger']) && $options['logger'] instanceof LoggerInterface) {
+            $handlerStack->push(Middleware::log(
+                $options['logger'],
+                new MessageFormatter(
+                    '{hostname} {req_header_User-Agent} - [{ts}] "{method} {resource} {protocol}/{version}"' .
+                    ' {code} {res_header_Content-Length}'
+                )
+            ));
+        }
+        // finally create the instance
+        return new GuzzleClient(['base_url' => $url, 'handler' => $handlerStack]);
+    }
+
+    private static function createDefaultDecider(int $maxRetries): Closure
+    {
+        return function (
+            $retries,
+            RequestInterface $request,
+            ?ResponseInterface $response = null,
+            $error = null
+        ) use ($maxRetries) {
+            if ($retries >= $maxRetries) {
+                return false;
+            } elseif ($response && $response->getStatusCode() >= 500) {
+                return true;
+            } elseif ($error) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+    }
+
+    private function sendRequest(Request $request): array
+    {
+        try {
+            $response = $this->guzzle->send($request);
+            $data = json_decode($response->getBody()->getContents(), true);
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                throw new ClientException('Unable to parse response body into JSON: ' . json_last_error());
+            }
+            return $data ?: [];
+        } catch (GuzzleException $e) {
+            throw new ClientException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
+    public function getJobData(string $jobId): array
+    {
+        $request = new Request('GET', 'jobs/' . $jobId);
+        return $this->sendRequest($request);
+    }
+
+    public function postJobResult(string $jobId, array $result): array
+    {
+        $request = new Request('POST', 'jobs/' . $jobId, [], json_encode($result));
+        return $this->sendRequest($request);
+    }
+}
