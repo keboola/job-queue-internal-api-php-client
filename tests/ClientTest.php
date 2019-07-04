978d94982007202ffa0cfe9d7cb7b66ed6b67f7b
odin <odinuv@gmail.com>
2019-07-04 12:06:44 +0200

    feat: getJobsWithStatus methods tests: more tests

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -10,10 +10,24 @@ use GuzzleHttp\Middleware;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Keboola\JobQueueInternalClient\Client;
+use Keboola\JobQueueInternalClient\JobFactory;
+use Keboola\ObjectEncryptor\ObjectEncryptorFactory;
 use PHPUnit\Framework\TestCase;
 
 class ClientTest extends TestCase
 {
+    private function getJobFactory(): JobFactory
+    {
+        $storageClientFactory = new JobFactory\StorageClientFactory('http://example.com/');
+        $objectEncryptorFactory = new ObjectEncryptorFactory('alias/some-key', 'us-east-1', '', '');
+        return new JobFactory($storageClientFactory, $objectEncryptorFactory);
+    }
+
+    private function getClient(array $options): Client
+    {
+        return new Client($this->getJobFactory(), 'http://example.com/', 'dummy', $options);
+    }
+
     public function testGetJobData(): void
     {
         $mock = new MockHandler([
@@ -27,11 +41,11 @@ class ClientTest extends TestCase
                     },
                     "token": {
                         "id": "789",
-                        "token": "123-456-xxxxxxx"
+                        "token": "KBC::ProjectSecure::eJwBYAGf"
                     },
                     "params": {
                         "mode": "run",
-                        "component": "keboola.ex-db-snowflake",
+                        "component": "keboola.test",
                         "config": "123456",
                         "configData": {
                             "parameters": {
@@ -43,17 +57,20 @@ class ClientTest extends TestCase
             ),
         ]);
         // Add the history middleware to the handler stack.
-        $container = [];
-        $history = Middleware::history($container);
+        $requestHistory = [];
+        $history = Middleware::history($requestHistory);
         $stack = HandlerStack::create($mock);
         $stack->push($history);
-        $client = new Client('http://example.com/', 'testToken', ['handler' => $stack]);
-        $data = $client->getJobData('123');
-        self::assertEquals('123', $data['id']);
-        self::assertEquals('123456', $data['params']['config']);
-        self::assertCount(1, $container);
+        $client = $this->getClient(['handler' => $stack]);
+        $job = $client->getJob('123');``
+        self::assertEquals('123', $job->getId());
+        self::assertEquals('123456', $job->getConfigId());
+        self::assertEquals('keboola.test', $job->getComponentId());
+        self::assertStringStartsWith('KBC::ProjectSecure::', $job->getToken());
+        self::assertEquals(['parameters' => ['foo' => 'bar']], $job->getConfigData());
+        self::assertCount(1, $requestHistory);
         /** @var Request $request */
-        $request = $container[0]['request'];
+        $request = $requestHistory[0]['request'];
         self::assertEquals('jobs/123', $request->getUri()->__toString());
         self::assertEquals('GET', $request->getMethod());
         self::assertEquals('testToken', $request->getHeader('X-InternalApi-Token')[0]);
@@ -76,14 +93,14 @@ class ClientTest extends TestCase
         $stack = HandlerStack::create($mock);
         $stack->push($history);
         $client = new Client('http://example.com/', 'testToken', ['handler' => $stack]);
-        $client->postJobResult('123', ['images' => ['digests' => ['keboola.ex-db-snowflake' => ['id' => '123']]]]);
+        $client->postJobResult('123', ['images' => ['digests' => ['keboola.test' => ['id' => '123']]]]);
         self::assertCount(1, $container);
         /** @var Request $request */
         $request = $container[0]['request'];
         self::assertEquals('jobs/123', $request->getUri()->__toString());
         self::assertEquals('POST', $request->getMethod());
         self::assertEquals(
-            '{"images":{"digests":{"keboola.ex-db-snowflake":{"id":"123"}}}}',
+            '{"images":{"digests":{"keboola.test":{"id":"123"}}}}',
             $request->getBody()->getContents()
         );
         self::assertEquals('testToken', $request->getHeader('X-InternalApi-Token')[0]);
