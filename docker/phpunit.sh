80de22056cd55b3b5ade2c258cc23d327906767a
odin <odinuv@gmail.com>
2019-07-26 13:27:18 +0200

    feat: remove test dependency on remote queue API feat: remove test dependency on shared KMS key docs: updated local setup instructions fix: remove hardcoded values from build pipeline fix: build on self-hosted azure

---
+++ docker/phpunit.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+/code/docker/wait-for-it.sh --strict --timeout=120 internal-api:3000
+curl -X POST http://internal-api:3000/index -H 'Content-Type: application/json' -d '{"index": "syrup"}'
+composer ci
