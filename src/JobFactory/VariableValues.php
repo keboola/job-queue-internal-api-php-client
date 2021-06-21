228bad7b13f4b082e61749841fdbe21c0905455d
odin <odinuv@gmail.com>
2021-06-21 23:49:28 +0200

    feat: resolve overriding values

---
+++ src/JobFactory/VariableValues.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\JobQueueInternalClient\JobFactory;
+
+use Keboola\JobQueueInternalClient\Exception\ClientException;
+
+class VariableValues
+{
+    /** @var string|null */
+    private $variableValuesId;
+    /** @var array */
+    private $variableValuesData;
+
+    public function __construct(?string $variableValuesId, array $variableValuesData)
+    {
+        if (!empty($variableValuesId) && !empty($variableValuesData)) {
+            throw new ClientException(
+                'Provide either "variableValuesId" or "variableValuesData", but not both.'
+            );
+        }
+
+        $this->variableValuesId = $variableValuesId;
+        $this->variableValuesData = $variableValuesData;
+    }
+
+    public static function fromDataArray(array $data): self
+    {
+        return new self(
+            $data['variableValuesId'] ?? null,
+            $data['variableValuesData'] ?? []
+        );
+    }
+
+    public function asDataArray(): array
+    {
+        $data = [];
+        if ($this->variableValuesId) {
+            $data['variableValuesId'] = $this->variableValuesId;
+        }
+        if ($this->variableValuesData) {
+            $data['variableValuesData'] = $this->variableValuesData;
+        }
+        return $data;
+    }
+
+    public function getValuesId(): ?string
+    {
+        return $this->variableValuesId;
+    }
+
+    public function getValuesData(): array
+    {
+        return $this->variableValuesData;
+    }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->getValuesId()) && empty($this->getValuesData());
+    }
+}
---
+++ tests/JobFactory/OverridesConfigurationDefinitionTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Keboola\JobQueueInternalClient\Tests\JobFactory;
+
+use Keboola\JobQueueInternalClient\JobFactory\NewJobDefinition;
+use Keboola\JobQueueInternalClient\JobFactory\OverridesConfigurationDefinition;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+
+class OverridesConfigurationDefinitionTest extends TestCase
+{
+    public function testValidOverrideMinimal(): void
+    {
+        $data = [];
+        $definition = new OverridesConfigurationDefinition();
+        $processed = $definition->processData($data);
+        self::assertEquals([], $processed);
+    }
+
+    public function testValidOverrideFull(): void
+    {
+        $data = [
+            'extra' => 'removed',
+            'variableValuesData' => [
+                'values' => [
+                    [
+                        'name' => 'foo',
+                        'value' => 'bar',
+                    ],
+                ],
+            ],
+            'variableValuesId' => 123,
+            'runtime' => [
+                'also' => 'removed',
+                'tag' => 1,
+                'backend' => [
+                    'type' => 'weird',
+                ],
+            ],
+        ];
+        $definition = new OverridesConfigurationDefinition();
+        $expected = $data;
+        unset($expected['extra']);
+        unset($expected['runtime']['also']);
+        $expected['variableValuesId'] = '123';
+        $expected['runtime']['tag'] = '1';
+        self::assertSame($expected, $definition->processData($data));
+    }
+
+    public function invalidConfigurationProvider(): array
+    {
+        return [
+            'Invalid Variable Values Data' => [
+                [
+                    'variableValuesData' => '5',
+                ],
+                '#Invalid type for path "overrides.variableValuesData". Expected "?array"?, but got "?string"?#',
+            ],
+            'Invalid variable values ID' => [
+                [
+                    'variableValuesId' => ['123'],
+                    'runtime' => [
+                        'tag' => '1.2.3',
+                    ],
+                ],
+                '#Invalid type for path "overrides.variableValuesId". Expected "?scalar"?, but got "?array"?#',
+            ],
+            'Invalid tag' => [
+                [
+                    'runtime' => [
+                        'tag' => ['1.2.3'],
+                    ],
+                ],
+                '#Invalid type for path "overrides.runtime.tag". Expected "?scalar"?, but got "?array"?#',
+            ],
+            'Invalid backend' => [
+                [
+                    'runtime' => [
+                        'backend' => [
+                            'type' => ['weird'],
+                        ],
+                    ],
+                ],
+                '#Invalid type for path "overrides.runtime.backend.type". Expected "?scalar"?, but got "?array"?#',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidConfigurationProvider
+     * @param array $jobData
+     * @param string $message
+     */
+    public function testInvalidConfigurationOverride(array $jobData, string $message): void
+    {
+        self::expectException(InvalidConfigurationException::class);
+        self::expectExceptionMessageMatches($message);
+        $definition = new OverridesConfigurationDefinition();
+        $definition->processData($jobData);
+    }
+}
