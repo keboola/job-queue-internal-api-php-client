c7b8bbfde9a22af8179183f55f83601710689609
Erik Zigo <erik.zigo@keboola.com>
2021-09-20 10:26:21 +0200

    JobFactory/Mapping/TableCollection

---
+++ src/JobFactory/Mapping/TableCollection.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\JobQueueInternalClient\JobFactory\Mapping;
+
+use Countable;
+use Generator;
+use IteratorAggregate;
+
+class TableCollection implements IteratorAggregate, Countable
+{
+    /** @var Table[] */
+    private array $items = [];
+
+    /**
+     * @return Generator<int, Table>
+     */
+    public function getIterator(): Generator
+    {
+        foreach ($this->items as $item) {
+            yield $item;
+        }
+    }
+
+    public function count(): int
+    {
+        return count($this->items);
+    }
+
+    public function jsonSerialize(): array
+    {
+        return array_map(function (Table $table) {
+            return $table->jsonSerialize();
+        }, $this->items);
+    }
+
+    public function addTable(Table $table): self
+    {
+        $this->items[] = $table;
+        return $this;
+    }
+}
