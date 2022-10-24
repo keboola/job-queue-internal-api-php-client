4a3efc0ecf7001fbf18c8dc443be8001d5e340d5
Erik Zigo <erik.zigo@keboola.com>
2022-10-24 14:08:02 +0200

    internal-api-client - JobFactory/Backend with context option

--- tests/JobFactory/BackendTest.php
+++ tests/JobFactory/BackendTest.php
@@ -11,20 +11,22 @@ class BackendTest extends TestCase
 {
     public function typesProvider(): iterable
     {
-        yield 'null' => [null, null];
-        yield 'foo' => ['foo', null];
-        yield 'containerFoo' => [null, 'foo'];
-        yield 'FooBoo' => ['foo', 'boo'];
+        yield 'null' => [null, null, null];
+        yield 'foo' => ['foo', null, null];
+        yield 'containerFoo' => [null, 'foo', null];
+        yield 'contextFoo' => [null, null, 'foo'];
+        yield 'FooBooLoo' => ['foo', 'boo', 'loo'];
     }
 
     /**
      * @dataProvider typesProvider
      */
-    public function testCreate(?string $type, ?string $containerType): void
+    public function testCreate(?string $type, ?string $containerType, ?string $context): void
     {
-        $backend = new Backend($type, $containerType);
+        $backend = new Backend($type, $containerType, $context);
         self::assertSame($type, $backend->getType());
         self::assertSame($containerType, $backend->getContainerType());
+        self::assertSame($context, $backend->getContext());
     }
 
     /**
@@ -34,21 +36,64 @@ class BackendTest extends TestCase
         array $data,
         ?string $expectedType,
         ?string $expectedContainerType,
+        ?string $expectedContext,
         bool $expectedEmpty
     ): void {
         $backend = Backend::fromDataArray($data);
 
         self::assertSame($expectedType, $backend->getType());
         self::assertSame($expectedContainerType, $backend->getContainerType());
+        self::assertSame($expectedContext, $backend->getContext());
         self::assertSame($expectedEmpty, $backend->isEmpty());
     }
 
     public function provideCreateFromArrayData(): iterable
     {
-        yield 'empty' => [[], null, null, true];
-        yield 'with type' => [['type' => 'custom'], 'custom', null, false];
-        yield 'with container type' => [['containerType' => 'custom'], null, 'custom', false];
-        yield 'with both types' => [['type' => 'custom', 'containerType' => 'motsuc'], 'custom', 'motsuc', false];
+        yield 'empty' => [
+            [],
+            null,
+            null,
+            null,
+            true,
+        ];
+        yield 'with type' => [
+            [
+                'type' => 'custom',
+            ],
+            'custom',
+            null,
+            null,
+            false,
+        ];
+        yield 'with container type' => [
+            [
+                'containerType' => 'custom',
+            ],
+            null,
+            'custom',
+            null,
+            false,
+        ];
+        yield 'with context' => [
+            [
+                'context' => 'wlm',
+            ],
+            null,
+            null,
+            'wlm',
+            false,
+        ];
+        yield 'with all properties' => [
+            [
+                'type' => 'custom',
+                'containerType' => 'motsuc',
+                'context' => 'wlm',
+            ],
+            'custom',
+            'motsuc',
+            'wlm',
+            false,
+        ];
     }
 
     /**
@@ -62,20 +107,44 @@ class BackendTest extends TestCase
     public function provideExportAsDataArrayData(): iterable
     {
         yield 'empty' => [
-            new Backend(null, null),
-            ['type' => null, 'containerType' => null],
+            new Backend(null, null, null),
+            [
+                'type' => null,
+                'containerType' => null,
+                'context' => null,
+            ],
         ];
         yield 'with type' => [
-            new Backend('custom', null),
-            ['type' => 'custom', 'containerType' => null],
+            new Backend('custom', null, null),
+            [
+                'type' => 'custom',
+                'containerType' => null,
+                'context' => null,
+            ],
         ];
         yield 'with container type' => [
-            new Backend(null, 'custom'),
-            ['type' => null, 'containerType' => 'custom'],
+            new Backend(null, 'custom', null),
+            [
+                'type' => null,
+                'containerType' => 'custom',
+                'context' => null,
+            ],
         ];
-        yield 'with both types' => [
-            new Backend('custom', 'motsuc'),
-            ['type' => 'custom', 'containerType' => 'motsuc'],
+        yield 'with context' => [
+            new Backend(null, null, 'wml'),
+            [
+                'type' => null,
+                'containerType' => null,
+                'context' => 'wml',
+            ],
+        ];
+        yield 'with all properties' => [
+            new Backend('custom', 'motsuc', 'wml'),
+            [
+                'type' => 'custom',
+                'containerType' => 'motsuc',
+                'context' => 'wml',
+            ],
         ];
     }
 }
