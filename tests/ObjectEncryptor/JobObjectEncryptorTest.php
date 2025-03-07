24cd83232a93b5e7545a9e882d39f5b9fcf36fa8
Pepa Martinec <josef.martinec@keboola.com>
2025-03-07 16:33:24 +0100

    feat[internal-api-php-client]: Unify JobObjectEncryptor with the one in docker-bundle

--- tests/ObjectEncryptor/JobObjectEncryptorTest.php
+++ tests/ObjectEncryptor/JobObjectEncryptorTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Keboola\JobQueueInternalClient\Tests\ObjectEncryptor;
 
+use InvalidArgumentException;
 use Keboola\JobQueueInternalClient\JobFactory\JobObjectEncryptor;
 use Keboola\ObjectEncryptor\ObjectEncryptor;
 use Keboola\PermissionChecker\BranchType;
@@ -11,7 +12,7 @@ use PHPUnit\Framework\TestCase;
 
 class JobObjectEncryptorTest extends TestCase
 {
-    public function testEncryptWithoutBranch(): void
+    public function testEncryptWithoutBranchAndNoProtectedFeature(): void
     {
         $internalEncryptor = $this->createMock(ObjectEncryptor::class);
         $internalEncryptor->expects(self::once())
@@ -21,12 +22,45 @@ class JobObjectEncryptorTest extends TestCase
         ;
 
         $encryptor = new JobObjectEncryptor($internalEncryptor);
-        $result = $encryptor->encrypt('data', 'componentId', 'projectId', null);
+        $result = $encryptor->encrypt('data', 'componentId', 'projectId', null, []);
 
         self::assertSame('encryptedData', $result);
     }
 
-    public function testEncryptWithBranch(): void
+    public function testEncryptWithBranchAndNoProtectedFeature(): void
+    {
+        $internalEncryptor = $this->createMock(ObjectEncryptor::class);
+        $internalEncryptor->expects(self::once())
+            ->method('encryptForProject')
+            ->with('data', 'componentId', 'projectId')
+            ->willReturn('encryptedData')
+        ;
+
+        $encryptor = new JobObjectEncryptor($internalEncryptor);
+        $result = $encryptor->encrypt('data', 'componentId', 'projectId', BranchType::DEFAULT, []);
+
+        self::assertSame('encryptedData', $result);
+    }
+
+    public function testEncryptWithProtectedDefaultBranchFeatureAndNoBranch(): void
+    {
+        $internalEncryptor = $this->createMock(ObjectEncryptor::class);
+        $internalEncryptor->expects(self::never())
+            ->method('encryptForProject')
+        ;
+        $internalEncryptor->expects(self::never())
+            ->method('encryptForBranchType')
+        ;
+
+        $encryptor = new JobObjectEncryptor($internalEncryptor);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Protected default branch feature is enabled, but branch type is not set.');
+
+        $encryptor->encrypt('data', 'componentId', 'projectId', null, ['protected-default-branch']);
+    }
+
+    public function testEncryptWithProtectedDefaultBranchFeatureAndBranch(): void
     {
         $internalEncryptor = $this->createMock(ObjectEncryptor::class);
         $internalEncryptor->expects(self::once())
@@ -36,7 +70,13 @@ class JobObjectEncryptorTest extends TestCase
         ;
 
         $encryptor = new JobObjectEncryptor($internalEncryptor);
-        $result = $encryptor->encrypt('data', 'componentId', 'projectId', BranchType::DEFAULT);
+        $result = $encryptor->encrypt(
+            'data',
+            'componentId',
+            'projectId',
+            BranchType::DEFAULT,
+            ['protected-default-branch'],
+        );
 
         self::assertSame('encryptedData', $result);
     }
