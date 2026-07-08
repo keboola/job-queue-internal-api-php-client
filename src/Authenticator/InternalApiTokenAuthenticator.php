c2fae89c3d142504b8194b2397a853c5ed47cbbf
Pepa Martinec <josef.martinec@keboola.com>
2026-07-08 13:19:25 +0200

    feat(internal-api-php-client): add InternalApiTokenAuthenticator (PAT-1839)

---
+++ src/Authenticator/InternalApiTokenAuthenticator.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\JobQueueInternalClient\Authenticator;
+
+use Keboola\ApiClientBase\Auth\RequestAuthenticatorInterface;
+use Psr\Http\Message\RequestInterface;
+use SensitiveParameter;
+use Webmozart\Assert\Assert;
+
+final readonly class InternalApiTokenAuthenticator implements RequestAuthenticatorInterface
+{
+    public const HEADER = 'X-JobQueue-InternalApi-Token';
+
+    /**
+     * @param non-empty-string $token
+     */
+    public function __construct(
+        #[SensitiveParameter]
+        private string $token,
+    ) {
+        Assert::stringNotEmpty($token, 'Internal API token must not be empty');
+    }
+
+    public function __invoke(RequestInterface $request): RequestInterface
+    {
+        return $request->withHeader(self::HEADER, $this->token);
+    }
+}
---
+++ tests/Authenticator/InternalApiTokenAuthenticatorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\JobQueueInternalClient\Tests\Authenticator;
+
+use GuzzleHttp\Psr7\Request;
+use InvalidArgumentException;
+use Keboola\ApiClientBase\Auth\RequestAuthenticatorInterface;
+use Keboola\JobQueueInternalClient\Authenticator\InternalApiTokenAuthenticator;
+use PHPUnit\Framework\TestCase;
+
+class InternalApiTokenAuthenticatorTest extends TestCase
+{
+    public function testAddsHeader(): void
+    {
+        $authenticator = new InternalApiTokenAuthenticator('my-token');
+        self::assertInstanceOf(RequestAuthenticatorInterface::class, $authenticator);
+
+        $request = $authenticator(new Request('GET', 'https://example.com/'));
+
+        self::assertSame('my-token', $request->getHeaderLine('X-JobQueue-InternalApi-Token'));
+    }
+
+    public function testEmptyTokenThrows(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new InternalApiTokenAuthenticator(''); // @phpstan-ignore-line
+    }
+}
